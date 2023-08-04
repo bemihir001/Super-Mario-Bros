bae7915727e2be777d69d6f428e0744ec1196af9
Mihir Parmar <99240627+bemihir001@users.noreply.github.com>
2023-08-04 14:48:23 +0530

    Project Files

---
+++ Assets/Scripts/PlayerSpriteRenderer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteRenderer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private PlayerMoment moment;
+
+    public Sprite idle;
+    public Sprite jump;
+    public Sprite slide;
+    public AnimatedSprite run;
+
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        moment = GetComponentInParent<PlayerMoment>();
+    }
+
+    private void OnEnable(){
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable(){
+        spriteRenderer.enabled = false;
+    }
+
+    private void LateUpdate(){
+        run.enabled = moment.running;
+        if(moment.jumping){
+            spriteRenderer.sprite = jump;
+        }
+        else if(moment.sliding){
+            spriteRenderer.sprite = slide;
+        }
+        else if(!moment.running){
+            spriteRenderer.sprite = idle;
+        }
+    }
+}
---
+++ Assets/Scripts/SideScrolling.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake(){
+        player = GameObject.FindWithTag("Player").transform;
+    }
+    // runs after mario's position updated!
+    private void LateUpdate(){
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+}
